c26a13d55a761ad56d9ca9b16c9aa27733b97c14
TomasS666 <punkertje66@gmail.com>
2019-05-23 10:16:34 +0200

    added project files

--- server.js
+++ server.js
@@ -1,9 +1,64 @@
 
 const http = require('http');
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
 
-http.createServer(onrequest).listen(8000);
+const app = express();
 
-function onrequest(req, res){
-  console.log('Aight');
-  res.end(req + res);
+
+const data = [
+{
+  name:'Tomas',
+  age: 23
+},
+{
+  name:'Marcella',
+  age: 50
+}
+];
+
+app
+.use(express.static('static'))
+.use(bodyParser.urlencoded({extended: true}))
+.set('view-engine', 'ejs')
+.set('views', 'views')
+.get(':var(/|/home)?', home)
+.get('/about', onabout)
+.get('/profile', profile)
+.post('/', addArtist)
+.get('*', (req, res)=>{
+  res.render('404.ejs');
+})
+.listen(8000, function(){
+  console.log('Listening over 8000');
+});
+
+function onabout(req, res){
+  res.render('test.ejs', {
+    data: data
+  });
+}
+
+function addArtist(req, res){
+  data.push({
+    name: req.body.title
+  });
+  res.render('objects.ejs', {
+    data: data,
+    title: ''
+  });
+}
+
+function home(req, res){
+  res.render('home.ejs', {
+    data: data
+  })
+}
+
+  function profile(req, res){
+    res.render('profile.ejs', {
+      data: data,
+      title: "My Profile"
+  })
 }
