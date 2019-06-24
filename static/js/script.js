22f0a7d2901d9429ffe85d2abe199d49037e8c45
TomasS666 <punkertje66@gmail.com>
2019-06-24 13:17:49 +0200

    finally!

--- static/js/script.js
+++ static/js/script.js
@@ -45,11 +45,75 @@ window.onload = function(){
   }
 
   // Removing bands
-  const deleteBtns = document.querySelectorAll('a[data-method="delete"]');
-  if(deleteBtns){
-    deleteBtns.addEventListener('click', (e)=>{
-      console.log(e.target.dataset.id)
+
+  // Checks if there are delete buttons with the data-method attribute = delete
+  if(document.querySelectorAll('button[data-method="delete"]')){
+  
+    // If so, select all those nodeelements and receive a nodelist binded to nodelist.
+    const nodelist = document.querySelectorAll('button[data-method="delete"]');
+
+    /* I want to call the forEach method on the nodelist. You can do that nowadays but to be rather sure
+      for browser support, I'm first gonna convert the nodelist to an instance of an array.
+    */
+    const deleteBtns = Array.from(nodelist);
+
+    /* Then I apply a foreach method on the array so I can apply the addEventListener to each of the items
+      inside the array. Once you click on one of the elements, the event target (so where you basically click on) it's data-attribute 
+      name is gonna be logged. I render the Object unique id there. So I can use this to delete the instance of that object from the user 
+      that's currently logged in.
+    */
+    deleteBtns.forEach((elem)=>{
+      elem.addEventListener('click', (event)=>{
+        console.log(event.target.dataset.id);
+        let confirmPopup = document.querySelector('.confirm-delete');
+        let confirmText = document.querySelector('.confirm-delete p');
+        let cancelBtn = document.querySelector('button[data-name="cancel"]');
+        let deleteConfirm = document.querySelector('button[data-name="delete"]');
+
+        confirmPopup.classList.add('show');
+
+        cancelBtn.addEventListener('click', (event)=>{
+          console.log(`canceled`)
+          confirmPopup.classList.remove('show');
+        })
+
+        confirmText.textContent = `Are you sure you want to delete ${event.target.dataset.name}?`;
+
+        let band = event.target.dataset.id;
+
+        // delete(event.target.dataset.id)
+        deleteConfirm.addEventListener('click', ()=>{
+          deleting(band);
+        })
+      })
     })
+
+
+    function deleting(band){
+      let confirmPopup = document.querySelector('.confirm-delete');
+
+        console.log(`deleting ${band}`)
+        confirmPopup.classList.remove('show');
+
+        // Gebruikt uit example
+        fetch('/top-twenty/' + band, {method: 'delete'})
+        .then(onresponse)
+        .then(onload, onfail)
+
+        function onresponse(res) {
+          return res.json()
+        }
+      
+        function onload() {
+          window.location = '/'
+        }
+      
+        function onfail() {
+          throw new Error('Could not delete!')
+        }
+
+    }
+
   }
 
 
